afb850e093513470aa0490c0c8bd7a81e6b97d15
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-02-27 21:30:35 -0600

    adding one more end point

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res) {
 	res.send('Hello there Nevil');
 });
 
+app.get('/data', function(req, res) {
+	res.json({
+		'name': 'Nevil George',
+		'age': 21,
+		'occupation': 'student'
+	});
+});
+
 app.listen(app.get('port'), function() {
 	console.log('App listening on port ' + app.get('port') + '...');
 });
